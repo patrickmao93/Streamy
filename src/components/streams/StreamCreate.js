1bc513432fe43acb4a6703e76851c737703b6bb5
Patrick <patrickmao93@gmail.com>
2018-12-25 03:54:37 +0800

    init; react-redux scaffolding; login with google button now works correctly

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamCreate = props => {
+  return <div>StreamCreate</div>;
+};
+
+export default StreamCreate;
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamEdit = props => {
+  return <div>StreamEdit</div>;
+};
+
+export default StreamEdit;
