258ff75d569e95690c65e6c91c9f5e48cb174817
Patrick <patrickmao93@gmail.com>
2018-12-26 15:33:09 +0800

    piled action type strings into types.js; added redux-form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 import reducers from "./reducers";
 import App from "./components/App";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware()))}>
     <App />
   </Provider>,
   document.getElementById("root")
