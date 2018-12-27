37093b177591b83ef89f0be3bd060831148fc234
Patrick <patrickmao93@gmail.com>
2018-12-27 15:46:02 +0800

    added dependency redux-thunk and axios; added create, fetch, delete, update actions

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
+
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,9 +26,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -57,7 +60,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 
 import reducers from "./reducers";
 import App from "./components/App";
@@ -9,7 +10,9 @@ import App from "./components/App";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware()))}>
+  <Provider
+    store={createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))}
+  >
     <App />
   </Provider>,
   document.getElementById("root")
