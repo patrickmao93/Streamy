9507f8ccd11733c6a38b72cfa9101a6908e78118
Patrick <patrickmao93@gmail.com>
2018-12-27 19:01:27 +0800

    the app now uses custom router history object instead of the one BrowserRouter depends on; added isolation to StreamEdit component. It now correctly shows stream with id provided in route when user navigate it directly

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 const App = props => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div className="">
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
-          <Route path="/streams/delete" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/show/:id" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
