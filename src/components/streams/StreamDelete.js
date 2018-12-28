a7b9676781be4b45bf9fc06bfe67d8092cf3d9f0
Patrick <patrickmao93@gmail.com>
2018-12-28 15:45:38 +0800

    stream deleting modal now shows up when user clicks on delete button in stream list component; Stilling working on deletion logic

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,56 @@
 import React from "react";
+import { connect } from "react-redux";
+import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream } from "../../actions";
 
-const StreamDelete = props => {
-  return <div>StreamDelete</div>;
+class StreamDelete extends React.Component {
+  renderContent = () => {
+    if (!this.props.stream) {
+      return "Loading...";
+    }
+    return (
+      "Are you sure you want to delete this stream? " + this.props.stream.title
+    );
+  };
+  renderActions = () => {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  };
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          header="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
+
+const onClick = () => {};
+
+const mapStateToProps = (state, ownProps) => {
+  console.log(state.streams);
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamDelete;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamDelete);
